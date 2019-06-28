4ecb8ad5fd750fb52f5df473e689d150b4bfc95f
Natalija Nikolova <45513903+natalijaN@users.noreply.github.com>
2019-06-28 16:01:00 +0200

    commit

--- dog-fiesta/src/store/reducers/projectReducer.js
+++ dog-fiesta/src/store/reducers/projectReducer.js
@@ -4,11 +4,10 @@ const initState = {
 
 const projectReducer = (state = initState, action) => {
     switch (action.type) {
-        case 'CREATE_PROJECT':
-            console.log('created project', action.project);
+        case 'CREATE_PROJECT':     
             return state;
         case 'CREATE_PROJECT_ERROR':
-            console.log('create project error', action.err);
+            console.log(action.err);
             return state;
         default:
             return state;
