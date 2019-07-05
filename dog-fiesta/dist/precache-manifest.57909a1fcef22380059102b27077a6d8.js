51b2be745266cdf37b37ba09084e4390a3a0038c
Natalija Nikolova <45513903+natalijaN@users.noreply.github.com>
2019-07-05 15:38:34 +0200

    commit

---
+++ dog-fiesta/dist/precache-manifest.57909a1fcef22380059102b27077a6d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd125291c4f8a10074eddd0b04196e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d65a14d10737dc64a64",
+    "url": "/static/css/main.d9b37430.chunk.css"
+  },
+  {
+    "revision": "4134d9e5d22f496afa65",
+    "url": "/static/js/2.6571d802.chunk.js"
+  },
+  {
+    "revision": "6d65a14d10737dc64a64",
+    "url": "/static/js/main.39ebfee4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
